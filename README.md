78177d8f14eb227e6be3a7b5025124004b98e30d
Chirag OPS Engineer <chirag01jain@gmail.com>
2016-11-24 17:22:57 +0530

    Script to move backup to dropbox

---
+++ README.md
@@ -0,0 +1,37 @@
+# backup-database-to-dropbox
+Take the backup of database and store it into your drop box account.
+
+#Setup steps
+To store the backup into drop box account, please download both the bash scripts dropbox_uploader.sh and db-backup.sh.
+
+Then give the execution permission to the script and run it:
+```BASH
+$ chmod +x dropbox_uploader.sh
+```
+Now, the script and you will get the a wizard in order to configure access to your Dropbox.
+```BASH
+$ ./dropbox_uploader.sh
+```
+Once setup is completed open the "backup-database.sh" and make the reqired changes in the parameters.
+```BASH
+Email address on which email needs to send.
+  EMAIL=""
+
+Database information for database backup
+  DB_USER=""
+  DB_PASS=""
+  DB_NAME=""
+
+Number of days to keep backup in dropbox
+  MAX_DAYS=""
+
+Path to the directory where dropbox_uploader.sh is located
+  UPLOAD_SCRIPT_DIR=""
+```
+After completing the above steps, set "backup-database.sh" in your crontab as per your requirements.
+
+# Dependencies
+1. One Email Server (postfix, sendmail, etc)
+2. Mutt utility.
+
+Thanks!
---
+++ db-backup.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+# Author: Chirag Jain
+# Description: The script is used to to take daily database backup and upload it to dropbox account.
+
+main() {
+	init
+	backupDatabase
+	sendEmail
+}
+
+init() {
+    # Email address on which email needs to send.
+    EMAIL=""
+
+    # Database information for database backup
+    DB_USER=""
+    DB_PASS=""
+    DB_NAME=""
+
+    # Number of days to keep backup in dropbox
+    MAX_DAYS=""
+
+    CURRENT_DATE=$(date +%d-%m-%Y)
+    LAST_DATE=$(date +%d-%m-%Y -d "$MAX_DAYS days ago")
+
+    NEW_FILE="$DB_NAME-$CURRENT_DATE.sql"
+    OLD_FILE="$DB_NAME-$LAST_DATE.sql.tar.gz"
+
+    # Path to the directory where dropbox_uploader.sh is located
+    UPLOAD_SCRIPT_DIR=""
+}
+
+backupDatabase() {
+
+    cd "$UPLOAD_SCRIPT_DIR"
+
+    mysqldump -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "$NEW_FILE"
+    if [[ ! -s "$NEW_FILE" ]]; then
+        unset STATUS
+        rm -rf "$NEW_FILE"
+    else
+        tar -czf "$NEW_FILE.tar.gz" "$NEW_FILE"
+        uploadToDropBox
+        removeFromDropBox
+        removeLocalBackup
+    fi
+}
+
+uploadToDropBox() {
+    sh dropbox_uploader.sh upload "$NEW_FILE.tar.gz" .
+    if [[ "$?" -eq 0 ]]; then
+        unset STATUS
+        STATUS="UPLOADED"
+    else
+        unset STATUS
+        STATUS="NOT_UPLOADED"
+    fi
+}
+
+removeFromDropBox() {
+	sh dropbox_uploader.sh delete "$OLD_FILE"
+}
+
+removeLocalBackup() {
+	rm -rf "$NEW_FILE"*
+}
+
+sendEmail() {
+	case "$STATUS" in
+	    FAIL_BACKUP )
+	        echo -e "Hi,\n\nBackup of database named '$DB_NAME' failed. Please take a manual backup for the database.\n\nThanks" | mutt -s "DB Backup Status" -- "$EMAIL"
+	        ;;
+
+	    UPLOADED )
+	        echo -e "Hi,\n\nBackup of database named '$DB_NAME' completed and uploaded to your dropbox account successfully.\n\nThanks" | mutt -s "DB Backup Status" -- "$EMAIL"
+	        ;;
+
+	    NOT_UPLOADED )
+	        echo -e "Hi,\n\nBackup of database named '$DB_NAME' completed but an error occured while uploading it to your dropbox account.\n\nThanks" | mutt -s "DB Backup Status" -- "$EMAIL"
+	        ;;
+	esac
+}
+
+main
